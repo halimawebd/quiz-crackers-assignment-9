1f1a4c67006cf078c988059593dfee6120bf6f48
halimawebd <halimawebd@gmail.com>
2022-10-30 19:24:12 +0600

    component home part

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import { createBrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 
 
-
+const router = createBrowserRouter([
+  
+]);
 
 
 function App() {
   return (
-   <Header></Header>
+  <div>
+<Header></Header>
  <Home></Home>
+  </div>
   
   );
 }
