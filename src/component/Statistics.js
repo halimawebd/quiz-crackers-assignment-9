241df739f0750f45d8556950c90a176c44ec6bc4
halimawebd <halimawebd@gmail.com>
2022-11-01 18:39:14 +0600

    footer part added

--- src/component/Statistics.js
+++ src/component/Statistics.js
@@ -1,54 +1,54 @@
-// import React, { useContext } from "react";
-// import { LoadContext } from "./Root";
+import React, { useContext } from "react";
+import { LoadContext } from "./Root";
 
-// import {
-//   LineChart,
-//   Line,
-//   XAxis,
-//   YAxis,
-//   CartesianGrid,
-//   Tooltip,
-//   Legend,
-// } from "recharts";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
-// const Statistics = () => {
-//   const rechartData = useContext(LoadContext);
-//   console.log(rechartData)
+const Statistics = () => {
+  const rechartData = useContext(LoadContext);
+  console.log(rechartData)
 
-//   return (
-//     <div>
+  return (
+    <div>
 
-//     <p className="text-center mt-[50px] text-xl  font-bold">This is recharts</p>
+    <p className="text-center mt-[50px] text-xl  font-bold">This is recharts</p>
   
-//     <div className="flex justify-center mt-5">
+    <div className="flex justify-center mt-5">
      
-//       <LineChart
-//         width={500}
-//         height={300}
-//         data={rechartData}
-//         margin={{
-//           top: 5,
-//           right: 30,
-//           left: 20,
-//           bottom: 5,
-//         }}
-//       >
-//         <CartesianGrid strokeDasharray="3 3" />
-//         <XAxis dataKey="name" />
-//         <YAxis />
-//         <Tooltip />
-//         <Legend />
-//         <Line
-//           type="monotone"
-//           dataKey="total"
-//           stroke="#8884d8"
-//           activeDot={{ r: 8 }}
-//         />
-//         <Line type="monotone" dataKey="total" stroke="#82ca9d" />
-//       </LineChart>
-//     </div>
-//     </div>
-//   );
-// };
+      <LineChart
+        width={500}
+        height={300}
+        data={rechartData}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="total"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+        <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+      </LineChart>
+    </div>
+    </div>
+  );
+};
 
-// export default Statistics;
+export default Statistics;
